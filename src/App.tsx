a04734e403612da2ef4db521d38d0685ef095a57
CleverCodder <m.saad4code@gmail.com>
2021-12-28 19:16:13 +0500

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ const App: React.FC = () => {
       <>
       <GlobalStyle />
       <Wrapper>
-      <h1>REACT QUIZ</h1>
+      <h1>REACT QUI</h1>
       {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
         <button className='start' onClick={startTrivia}>
         Start
